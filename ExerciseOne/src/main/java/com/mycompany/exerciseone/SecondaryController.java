b4e5256a92a00aad608e344e5a4515c95499b3ac
Tsepo Ntsoane <93659779+TSEPO-NTSOANE@users.noreply.github.com>
2023-02-23 09:19:28 +0200

    Add files via upload

---
+++ ExerciseOne/src/main/java/com/mycompany/exerciseone/SecondaryController.java
@@ -0,0 +1,5 @@
+package com.mycompany.exerciseone;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
---
+++ ExerciseOne/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.exerciseone {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.exerciseone to javafx.fxml;
+    exports com.mycompany.exerciseone;
+}
+*/
